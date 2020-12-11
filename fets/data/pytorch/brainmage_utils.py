af7e89282c31a6b02fb40985a38e4fce54f3cc3b
sarthakpati <sarthak.pati@hotmail.com>
2020-12-11 17:56:08 -0500

    added basics of the patch-based inference

--- fets/data/pytorch/brainmage_utils.py
+++ fets/data/pytorch/brainmage_utils.py
@@ -11,6 +11,9 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # This is a 3-clause BSD license as defined in https://opensource.org/licenses/BSD-3-Clause
 
+import os
+os.environ['TORCHIO_HIDE_CITATION_PROMPT'] = '1' # hides torchio citation request, see https://github.com/fepegar/torchio/issues/235
+
 import numpy as np
 import pandas as pd
 import os
@@ -25,6 +28,7 @@ from torch.autograd import Variable
 from batchgenerators.augmentations.spatial_transformations import augment_rot90, augment_mirroring
 from batchgenerators.augmentations.noise_augmentations import augment_gaussian_noise
 
+import torchio
 
 def completely_replace_entries(array, old_to_new):
     new_array = array.copy()
@@ -224,7 +228,30 @@ class TumorSegmentationDataset(Dataset):
             sample = {'features': self.normalize_by_channel(feature_array), 'gt' : label_array}
         elif self.use_case == "inference":
             original_input_shape = list(feature_array.shape)
-            feature_array = self.zero_pad(feature_array)
+            feature_array_normalized = self.normalize_by_channel(feature_array)
+
+            # patch-based inference
+            subject_dict = {}
+            for i in range(0, original_input_shape[0]): # unsure about the range
+                subject_dict[str(i)] = feature_array_normalized[i]
+            
+            grid_sampler = torchio.inference.GridSampler(torchio.Subject(subject_dict), self.psize)
+            patch_loader = torch.utils.data.DataLoader(grid_sampler, batch_size=1)
+            aggregator = torchio.inference.GridAggregator(grid_sampler)
+
+            for patches_batch in patch_loader:
+                image = torch.cat([patches_batch[str(i)][torchio.DATA] for i in range(0, original_input_shape[0])], dim=1).cuda()
+                locations = patches_batch[torchio.LOCATION]
+                image = image.float().to(device) # this should happen where "device" is defined
+                pred_mask = model(image) # this should happen where "model" is defined
+                aggregator.add_batch(pred_mask, locations)
+            pred_mask = aggregator.get_output_tensor() # this is the final mask
+            # patch-based inference
+
+
+            ## no need for zero-padding for patch-based inference
+            # feature_array = self.zero_pad(feature_array)
+            
             # normalize features
             sample = {'features': self.normalize_by_channel(feature_array), 'metadata':  {"dir_path": dir_path, 
                                                                                           "original_x_dim": original_input_shape[1], 
